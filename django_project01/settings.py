e13bee889483b5452767c1055488066e6c4807b3
Hardik Jain <hardikjain8583@gmail.com>
2021-10-24 00:30:00 +0530

    heroku-1

--- django_project01/settings.py
+++ django_project01/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
@@ -130,4 +134,5 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
